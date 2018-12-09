a7e99c145c238a57e082d26943310246cf1283f3
waitme8888 <waitme8888@163.com>
2018-12-09 21:20:37 +0800

    test11

--- src/main/java/com/waite/gittest/Test.java
+++ src/main/java/com/waite/gittest/Test.java
@@ -3,6 +3,6 @@ package com.waite.gittest;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git test!");
+        System.out.println("王天棋是个哭吧精！！！");
     }
 }
