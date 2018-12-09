8ae1e960f03334cd53e665063ce4188c78b3c995
waitme8888 <waitme8888@163.com>
2018-12-09 21:35:28 +0800

    test2

--- src/main/java/com/waite/gittest/Test.java
+++ src/main/java/com/waite/gittest/Test.java
@@ -4,5 +4,6 @@ package com.waite.gittest;
 public class Test {
     public static void main(String[] args) {
         System.out.println("git test!");
+        System.out.println("ssssssss");
     }
 }
