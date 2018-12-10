6a4afdfdcffd97c974d46e63ca0158201ccb9e26
waitme8888 <waitme8888@163.com>
2018-12-10 17:57:07 +0800

    ignore

--- src/main/java/com/waite/gittest/Test.java
+++ src/main/java/com/waite/gittest/Test.java
@@ -4,5 +4,7 @@ package com.waite.gittest;
 public class Test {
     public static void main(String[] args) {
         System.out.println("王天棋是个哭吧精！！！");
+        System.out.println("git test!");
+        System.out.println("ssssssss");
     }
 }
