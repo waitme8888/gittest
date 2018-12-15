b24d92a7da82ef5f48f368821cdc50c18207d2da
waitme8888 <waitme8888@163.com>
2018-12-15 22:14:15 +0800

    modify dev

--- src/main/java/com/waite/gittest/Test.java
+++ src/main/java/com/waite/gittest/Test.java
@@ -3,8 +3,6 @@ package com.waite.gittest;
 
 public class Test {
     public static void main(String[] args) {
-
-        System.out.println("test!!!");
-
+        System.out.println("dev");
     }
 }
